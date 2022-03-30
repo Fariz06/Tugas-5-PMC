84fdbf763330db4d4136e296157e2d56530bc385
Fariz06 <98037750+Fariz06@users.noreply.github.com>
2022-03-30 20:24:02 +0700

    Update Naive Algorithm (belom rapih).c

--- Naive Algorithm (belom rapih).c
+++ Naive Algorithm (belom rapih).c
@@ -5,7 +5,11 @@
 int main(){
     int baris, kolom;
     time_t t;
-    scanf("%d %d", &baris, &kolom);
+    printf("Masukkan baris matriks : \n");
+    scanf("%d", &baris);
+    printf("Masukkan kolom matriks : \n");
+    scanf("%d",&kolom);
+
     int **matrix1 = (int**)malloc(sizeof(int*) * baris), **matrix2 = (int**)malloc(sizeof(int*) * baris), **matrix3 = (int**)malloc(sizeof(int*) * baris);
     srand((unsigned) time(&t));
 
@@ -15,12 +19,15 @@ int main(){
         matrix3[i] = (int*)calloc(baris, sizeof(int));
     }
 
+    // Mengisi matriks 1 dengan nilai random, dengan range 0-5
     for (int i = 0; i < baris; i++) {
         for (int j = 0; j < kolom; j++) {
             matrix1[i][j] = rand() % 5;
         }
     }
 
+    // Menampilkan isi dari matriks 1 untuk pengecekan manual hasil perkalian
+    printf("Isi dari matriks 1 adalah : \n");
     for (int i = 0; i < baris; i++){
        for (int j = 0; j < kolom; j++){
             printf("%d ", matrix1[i][j]);
@@ -29,11 +36,14 @@ int main(){
     }
     printf("=============================\n");
 
+    // Mengisi matriks 1 dengan nilai random, dengan range 0-5
     for (int i = 0; i < baris; i++) {
         for (int j = 0; j < kolom; j++) {
             matrix2[i][j] = rand() % 5;
         }
     }
+    printf("Isi dari matriks 2 adalah : \n");
+    // Menampilkan isi dari matriks 1 untuk pengecekan manual hasil perkalian
 
     for (int i = 0; i < baris; i++){
         for (int j = 0; j < kolom; j++){
@@ -43,6 +53,8 @@ int main(){
     }
     printf("=============================\n");
 
+
+    // Algoritma perkalian matriks menggunakan naive / iterative algorithm
     for (int i = 0; i < baris; i++){
         for (int j = 0; j < kolom; j++){
             matrix3[i][j] = 0;
@@ -52,12 +64,20 @@ int main(){
         }
     }
 
+    // Menampilkan matriks hasil perkalian matriks 1 dan matriks 2
+    printf("Hasil kali matriks 1 dan matriks 2 adalah \n");
     for (int i = 0; i < baris; i++){
         for (int j = 0; j < kolom; j++){
             printf("%d ", matrix3[i][j]);
         }
         printf("\n");
     }
+
+
+    // Membebaskan memory yang digunakan
+    free(matrix1);
+    free(matrix2);
+    free(matrix3);
 
     return 0;
 }
